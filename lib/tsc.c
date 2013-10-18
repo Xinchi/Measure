8c5fb3fb110713750d4724a6f32f6b8d1f8cf00b
popacai <popacai@popacai-ThinkPad-R400.(none)>
2013-10-19 03:17:00 +0800

    modified

---
+++ lib/tsc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#define DEBUG 1
+inline unsigned long tsc(void)
+{
+    register unsigned int lo, hi;
+    asm volatile ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((unsigned long)hi << 32UL) | (unsigned long)lo;
+}
+
+#ifdef DEBUG
+int main()
+{
+    unsigned long t1, t2;
+    //sleep (1);
+    t1 = tsc();
+    t2 = tsc();
+    printf("%ld,%ld,diff=%ld\n",t1, t2, t2 - t1);
+}
+#endif
