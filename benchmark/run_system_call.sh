41e171877dec70069c5ad7310b46ad33010b96ce
root <root@popacai-ThinkPad-R400.(none)>
2013-10-24 15:02:14 +0800

    fflush

--- benchmark/run_system_call.sh
+++ benchmark/run_system_call.sh
@@ -1,3 +1,5 @@
 python benchmark.py ../system_call/tgetpid > tgetpid.log
 python benchmark.py ../system_call/tgetppid > tgetppid.log
-python benchmark.py ../system_call/tdup > tdup.log
+python benchmark.py ../system_call/sgetpid > sgetpid.log
+python benchmark.py ../system_call/sgetppid > sgetppid.log
+#python benchmark.py ../system_call/tdup > tdup.log
