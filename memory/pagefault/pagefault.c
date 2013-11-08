45ea42ae14d5275d5c0c3c814ae43f45ef152854
root <root@popacai-ThinkPad-R400.(none)>
2013-11-08 15:44:35 +0800

    pagefault added

--- memory/pagefault/pagefault.c
+++ memory/pagefault/pagefault.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include "bw.h"
 
 unsigned long get_tsc(void)
 {
@@ -15,47 +16,37 @@ unsigned long get_tsc(void)
 
 int main()
 {
-    int fdin, fdout;
-    //void *src, *dst;
-    char* dst;
-    struct stat statbuf;
+    int fd;
     unsigned long t1, t2;
-    long time_sun;
-    //fdin = open("./Makefile", O_RDLY);
-    //fdout = open("./abc", O_RDWR );
-    //fdout = open("./abc", O_RDWR| O_CREAT | O_TRUNC );
-    //fdout = open("./abc", O_RDWR);
-    //
-    //
-    //
-    //fdout = open("/home/popacai/ram/abc", O_RDONLY);
-    fdout = open("./cba", O_RDONLY);
-
-    //lseek(fdout, 1000000 - 1, SEEK_SET);
-    //write(fdout,"", 1);
+    unsigned char* dst;
+    int size, chunksize;
+    size = 1024 * 1024;
+    chunksize = 1024;
+    fd = open("/home/popacai/ram/ram", O_RDONLY);
 
 
-    //dst = mmap(0,statbuf.st_size, PROT_READ | PROT_WRITE,MAP_FILE | MAP_SHARED, fdout, 0);
-    //dst = mmap(0, 1000000, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fdout, 0);
-    dst = mmap(0, 1000000, PROT_READ, MAP_FILE | MAP_SHARED, fdout, 0);
-    char a;
-    //memset(dst, 'a', 1000000);
-    time_sun = 0;
-    int i;
-	t1 = get_tsc();
-        a = dst[i];
-	t2 = get_tsc();
-	printf("%ld", t2 - t1);
-	/*
-    for (i = 0; i < (1000000); i+=100)
+    int pos;
+
+    pos = 0;
+    dst = mmap(size * pos , size * chunksize , PROT_READ, MAP_SHARED, fd, 0);
+
+
+    int i, j;
+    unsigned char tmp[1024];	
+    unsigned char c;
+    t1 = get_tsc();
+    for (i = 0; i < (size * chunksize); i += chunksize)
     {
-	time_sun += t2 - t1;
-	printf("%c\n",a);
+	//memcpy(tmp, dst+i, chunksize);
+	//memcpy_char;
+	c = dst[i];
     }
-    //printf("pos=%c\n", dst[0]);
-    printf("avg = %f", time_sun *1.0 / 1000);
-    */
-    close(fdout);
-    //memcpy(dst, src, statbuf.st_size);
+    t2 = get_tsc();
+    munmap(dst, size* chunksize);
+    //msync(dst, size* chunksize, MS_ASYNC);
+    //printf("%c\n", *(dst + size* chunksize - 1));
+    //printf("%ld\n", (t2 - t1));
+    printf("%ld\n", (t2 - t1) / size);
+    close(fd);
     exit(0);
 }
