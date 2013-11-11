7fd136a24a4810e5873bcb5e90671ae8d421a89a
Max <max@max-IdeaCentre-K450.(none)>
2013-11-10 20:28:53 -0800

    mem_read.c

--- memory/mem_read.c
+++ memory/mem_read.c
@@ -5,6 +5,9 @@
 #include <sys/resource.h>
 #include "math.h"
 #include <stdlib.h>
+
+#define TIMES 10
+#define REPETITION 1000000
 unsigned long get_tsc(void)
 {
     register unsigned int lo, hi;
@@ -14,90 +17,44 @@ unsigned long get_tsc(void)
 
 unsigned long diff_tsc(unsigned long t1, unsigned long t2)
 {
-    return (t2 - t1 - 67);
+    return (t2 - t1 -67);
 }
 
-#define TIMES 40
-#define STRIDE 4096
-#define HOT_SIZE 40
-#define TARGET_SIZE 320//kb
 
-int test()
+
+int main(int arvc, char* argv[])
 {
+	int num1,num2;
+	sscanf (argv[1],"%d",&num1);
+	sscanf (argv[2],"%d",&num2);
 	unsigned long t1;
 	unsigned long t2;
 	double diff;
 
-	//we put 20k in l2 size, and fill the l1 with all 8kb
-	int* l1; 
-	int* discontinue; 
-	int* l2;
-	int stride = STRIDE/4; //stride size = 1024.  
-	int array_size = TARGET_SIZE*1024/4;
-	l1 = malloc(1024*HOT_SIZE);
-	discontinue = malloc(1024*1024*3);
-	l2 = malloc(1024*TARGET_SIZE);
+//str is in byte, while size is in kb
 
-	int i;
-	for(i=0;i<HOT_SIZE*1024/4;i++)
+	int stride = num1/4;//stride = 4mb
+	int n;
+	for (n = 0;n<TIMES;n++)
 	{
-		l1[i] = 0;
+	        int size = (pow(2,num2)/4)*pow(2,10);//8kb
+	        int i = 0;
+	        //int a[array_size];
+	        int* a = malloc(size* sizeof (*a)); 
+	    
+	        for(;i<size;i++)
+	        {
+		    a[i] = (i+stride)%size;
+                	//	a[i] = 2;
+	        }
+	   
+	        int index =0;
+		int j = 0;
+		t1 = get_tsc();
+		for(j = 0;j<REPETITION;j++)
+		    index = a[index];
+		t2 = get_tsc();
+		diff = 1.0*diff_tsc(t1,t2)/REPETITION;
+		printf("%f\n",diff);
 	}
-	
-	for(i =0;i<array_size;i++)
-	{
-		l2[i] = (i+stride) % array_size;
-	}
-	
-	int index =0;
-	int j = 0;
-
-	//hot l1 and l2
-	for (i = 0; i < 10240; i++)
-	{
-		for(j = 0;j< (8 * 1024 / 4);j++)
-		{
-			index = l1[j];
-		}
-		for(j =0;j< (TARGET_SIZE*1024/4);j++)
-		{
-			index = l2[j];
-		}
-	}
-	//hot l1+l2
-	for(i=0;i<10240;i++)
-	{
-		for(j = 0;j< (HOT_SIZE * 1024 / 4);j++)
-		{
-			index = l1[j];
-		}
-	}
-
-	
-
-	
-
-	index = 0;
-	for (index = 10; index != 0; index--) // hot index
-	
-	t1 = get_tsc();
-	for(j = 0; j< TIMES; j++) // test 
-	    index = l2[index];
-	t2 = get_tsc();
-	diff = 1.0 * diff_tsc(t1,t2) / TIMES;
-	printf("%lf\n",diff);
-
-	free(l1);
-	free(l2);
-	free(discontinue);
-	return 0;
-}
-int main()
-{
-	int i;
-	for (i = 0; i < 15; i++)
-	{
-		test();
-	}
-	return 0;
 }
