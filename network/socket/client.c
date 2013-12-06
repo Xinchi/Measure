dac29ba3366c40c1a44323c4a75e8714f699eaf4
root <root@popacai-ThinkPad-R400.(none)>
2013-12-05 17:15:49 -1000

    hello

--- network/socket/client.c
+++ network/socket/client.c
@@ -6,6 +6,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <signal.h>
+int sockfd;
+void sig_handler(int signo)
+{
+    if (signo == SIGINT)
+    {
+	printf("signal\n");
+	close(sockfd);
+	exit(0);
+    }
+}
 
 void error(const char *msg)
 {
@@ -15,11 +26,16 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+    {
+	printf("not init the signal\n");
+	return 1;
+    }
+    int portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
+    char buffer[10240];
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -42,16 +58,13 @@ int main(int argc, char *argv[])
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
     printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
+    while (1)
+    {
+	bzero(buffer,10240);
+	n = write(sockfd,buffer,10240);
+    }
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
     close(sockfd);
     return 0;
 }
--- network/socket/server.c
+++ network/socket/server.c
@@ -7,7 +7,48 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <signal.h>
+#include <pthread.h>
 
+int sockfd, newsockfd;
+int sock_pool[64];
+pthread_t thread[64];
+int count;
+
+void sig_handler(int signo)
+{
+    int i;
+    if (signo == SIGINT)
+    {
+	printf("signal\n");
+	close(sockfd);
+	for (i = 0; i < count; i++)
+	{
+	    pthread_detach(thread[i]);
+	    close(sock_pool[i]);
+	}
+	exit(0);
+    }
+}
+
+void* receiver(void* input)
+{
+    int n;
+    char buffer[20480];
+    int socket;
+    socket = *(int*)input;
+    bzero(buffer,20480);
+    while (1)
+    {
+	n = read(socket,buffer,20480);
+	if (!n)
+	{
+	    break;
+	}
+	printf("%d\n", n);
+    }
+    printf("close connection\n");
+}
 void error(const char *msg)
 {
     perror(msg);
@@ -16,7 +57,13 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+    {
+	printf("not init the signal\n");
+	return 1;
+    }
+     int portno;
+     count = 0;
      socklen_t clilen;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
@@ -36,20 +83,22 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-     listen(sockfd,5);
+     listen(sockfd,10);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
+     while (1)
+     {
+	 newsockfd = accept(sockfd, 
+		     (struct sockaddr *) &cli_addr, 
+		     &clilen);
+	 printf("new client\n");
+	 if (newsockfd < 0) 
+	      error("ERROR on accept");
+	 sock_pool[count] = newsockfd;
+	 pthread_create(&(thread[count]), 0, receiver, 
+			&(sock_pool[count]));
+	 //bzero(buffer,256);
+	 //n = read(newsockfd,buffer,255);
+	 count++;
+     }
      return 0; 
 }
