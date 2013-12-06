32c186a8f6d2115c049957aaa940b3d8089031ab
Max GU <guxinchi2000@gmail.com>
2013-12-06 00:12:41 -0800

    max Dec 6

--- filesystem/file_cache.c
+++ filesystem/file_cache.c
@@ -18,36 +18,55 @@ unsigned long diff_tsc(unsigned long t1, unsigned long t2)
     return (t2 - t1 - 67);
 }
 
-#define BLOCKSIZE 10000
+#define BLOCKSIZE 10
 #define READTIME 100
 #define FILE_SIZE 1000000000
 int main()
 {
+	
 	unsigned long t1, t2;
 	int i;
-	int	fd = open("test.img", O_RDONLY, 0);
-	
-	for(int j = 0;j<READTIME;j++)
+	int fd = open("test.img", O_RDONLY, 0);
+	int bytesRead = 0;
+	int bytes;
+	long long j;
+	char* buf;
+	lseek(fd, FILE_SIZE-1,SEEK_SET);	
+	while(1)
 	{
-		read(fd, &i, sizeof(int));
+		lseek(fd, -2*BLOCKSIZE, SEEK_CUR);
+		bytes = read(fd, &j,BLOCKSIZE);
+//		printf("bytes = %d\n",bytes);
+ 		if (bytes <= 0 || bytesRead >= FILE_SIZE-2*BLOCKSIZE)
+                break;
+        bytesRead += bytes;
+//        printf("bytes read = %d\n",bytesRead); 
 	}
 	
-	t1 = get_tsc();
+	printf("done warming up \n");
+/*	
+	t1 = get_tsc();	
 	//operation
 	printf("%ld\n",t1);
 
 	lseek(fd, FILE_SIZE-1, SEEK_SET);
-	int bytesRead = 0;
+	bytesRead = 0;
 	printf("hello\n");
-	int bytes;
 	while(1){
-		lseek(fd, -2*BLOCKSIZE, SEEK_CUR); 
+		printf("entering loop\n");
+		lseek(fd, -2*BLOCKSIZE, SEEK_CUR);
+		printf("done lseek, now reading...\n"); 
 		bytes = read(fd, &i, BLOCKSIZE);
-//		printf("bytes = %d\n", bytes);
-		if (bytes <= 0 || bytesRead >= FILE_SIZE)
-       		break;
-    	bytesRead += bytes;
-//		printf("%d\n",bytesRead);
+		printf("done reading\n");
+		printf("bytes = %d\n", bytes);
+		if (bytes <= 0 || bytesRead >= FILE_SIZE-2*BLOCKSIZE)
+       			break;
+	    	bytesRead += bytes;
+		printf("bytes read = %d\n",bytesRead);
 	}
-	t2 = get_tsc();
+	printf("broke out\n");
+	return 0;
+
+//	t2 = get_tsc();
+*/
 }
